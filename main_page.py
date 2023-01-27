83fd753a2133d6eabe74008432fd0d15e953c7d0
Razbibul <bimmer02@yandex.ru>
2023-01-27 16:40:55 +0500

    new file

--- main_page.py
+++ main_page.py
@@ -2,7 +2,8 @@ from base_page import BasePage
 from selenium.webdriver.common.by import By
 
 
-class MainPage(BasePage): # Таким образом, класс MainPage будет иметь доступ ко всем атрибутам и методам своего класса-предка.
+
+class MainPage(BasePage): # Таким образом, класс MainPage будет иметь доступ ко всем атрибутам и методам своего класса-предка..
     def go_to_login_page(browser):
         login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
